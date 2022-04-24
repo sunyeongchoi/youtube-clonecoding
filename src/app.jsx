fc828220dc72106c29718e3d9d7a598a3dca982d
sn0716 <sn0716@naver.com>
2022-04-24 23:58:44 +0900

    youtube detail

--- src/app.jsx
+++ src/app.jsx
@@ -2,22 +2,40 @@ import React from 'react';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 import styles from './app.module.css';
+import VideoDetail from './components/video_detail/video_detail';
 
 function App({ youtube }) {
   const [videos, setVideos] = React.useState([]);
+  const [selectedVideo, setSelectedVideo] = React.useState(undefined);
 
   React.useEffect(() => {
     youtube.mostPopular().then(setVideos);
   }, [youtube]);
 
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  }
   const search = query => {
-    youtube.search(query).then(setVideos)
+    // setSelectedVideo(null);
+    youtube.search(query).then((videos) => {
+      setVideos(videos);
+      setSelectedVideo(null);
+    })
   }
 
   return (
     <div className={styles.app}>
       <SearchHeader onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList videos={videos} onVideoClick={selectVideo} display={selectedVideo ? 'list' : 'grid'} />
+        </div>
+      </section>
     </div>
   );
 }
