7a8f414e9501fe4522c793eee3c427c40bb076f5
sn0716 <sn0716@naver.com>
2022-04-25 23:45:03 +0900

    convert fetch to axios

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app';
-import Youtube from './service/youtube';
+import Youtube from './service/youtube-axios';
 
 const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 ReactDOM.render(
